aed3c868b4b208e6c724a19f6501c744e4ae740d
hnakae <nakae27@gmail.com>
2019-02-20 22:39:42 -0800

    add filter

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -3,8 +3,8 @@
 class ProductController {
 
     index({view}){
-        const users = [1,2,3,4,5,6,7,8,9,10];
-        return view.render('products/all', {users})
+        const products = [1,2,3,4,5,6,7,8,9,10];
+        return view.render('products/all', {products})
     }
 }
 
