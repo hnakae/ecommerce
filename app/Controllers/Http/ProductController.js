b4757c4ddd8546e4156f39d2034a8b470d460603
hnakae <nakae27@gmail.com>
2019-02-20 16:47:54 -0800

    all products page

--- app/Controllers/Http/ProductController.js
+++ app/Controllers/Http/ProductController.js
@@ -3,7 +3,8 @@
 class ProductController {
 
     index({view}){
-        return view.render('products/all')
+        const users = [1,2,3,4,5,6,7,8,9,10];
+        return view.render('products/all', {users})
     }
 }
 
